1f56c75b46d5d74631068ea32d52114fdeb5b64e
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 09:27:01 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greetings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name;");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "! Welcome to the program.");
+        }
+    }
+}
